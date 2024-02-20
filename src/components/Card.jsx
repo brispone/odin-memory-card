c834daa8f44904611eb80bb1f012c729673e457f
Bradley Rispone <bradrispone@gmail.com>
2024-02-20 13:16:49 -0600

    Add parallax tilt effect to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
+import Tilt from 'react-parallax-tilt';
 import '../styles/Card.css';
 
 function Card({ card }) {
 
 
     return (
-        <div className="card-front">
-            <img src={card.image.url} />
-            <h2 className="hero-name">{card.name}</h2>
+        <div className="tilt-wrapper">
+            <Tilt
+                tiltReverse
+                reset
+                /*glareEnable={true}
+                glareMaxOpacity={0.3}
+                glarePosition='all'*/
+                scale={1.1}>
+                <div className="card-front">
+                    <img src={card.image.url} />
+                    <h2 className="hero-name">{card.name}</h2>
+                </div>
+            </Tilt>
         </div>
     );
 }
