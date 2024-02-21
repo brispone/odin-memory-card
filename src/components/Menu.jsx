63ee198c88025f73a24c1b1277556210777afe1f
Bradley Rispone <bradrispone@gmail.com>
2024-02-21 15:59:20 -0600

    Fix spacing of directions on menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -16,6 +16,11 @@ function Menu({ startGame }) {
 
     return (
         <div className='menu'>
+            <div className='instructions'>
+                <h1>Multiverse Memory Matchup</h1>
+                <p>Don't select the same card twice. The higher the difficulty, the more cards there are to choose from. See how far you can go!</p>
+                <p>Select your difficulty:</p>
+            </div>
             <div className='btn-container'>
                 <button className='btn' onClick={startEasyGame}>EASY</button>
                 <button className='btn' onClick={startMediumGame}>MEDIUM</button>
